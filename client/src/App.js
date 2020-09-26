42649a8e7053c476c9c062dca4e43641b00241b7
Stephen Guo <wetech16@gmail.com>
2020-09-26 10:01:56 -0700

    -react setup

---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./App.css";
+import Signup from "./pages/Signup";
+
+function App() {
+  return <Signup />;
+}
+
+export default App;
---
+++ client/src/pages/Signup.js
@@ -0,0 +1,12 @@
+import React from "react";
+import InputWithIcon from "../components/IconsForm";
+
+function Signup() {
+  return (
+    <div>
+      <InputWithIcon />
+    </div>
+  );
+}
+
+export default Signup;
