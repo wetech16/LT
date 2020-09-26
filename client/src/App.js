0e1f967f71f31ebc8d3583d85be626b8dc7aa98a
Stephen Guo <wetech16@gmail.com>
2020-09-26 11:22:04 -0700

    -setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,31 @@
 import React from "react";
 import "./App.css";
 import Signup from "./pages/Signup";
+import {
+  createMuiTheme,
+  ThemeProvider,
+} from "@material-ui/core/styles";
+import { blue, lightBlue } from "@material-ui/core/colors";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      // Purple and green play nicely together.
+      main: blue[500],
+    },
+    secondary: {
+      // This is green.A700 as hex.
+      main: lightBlue[500],
+    },
+  },
+});
 
 function App() {
-  return <Signup />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Signup className="component"></Signup>
+    </ThemeProvider>
+  );
 }
 
 export default App;
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,12 +1,40 @@
 import React from "react";
-import InputWithIcon from "../components/IconsForm";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import "./signup.css";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+      width: "10ch",
+    },
+  },
+}));
 
 function Signup() {
+  const classes = useStyles();
   return (
-    <div>
-      <InputWithIcon />
+    <div className="class">
+      <form className={classes.root} noValidate autoComplete="off">
+        <TextField id="standard-basic" label="hello" />
+      </form>
+      <Button variant="contained" color="primary" className="button">
+        Primary
+      </Button>
+      <img src="" />
     </div>
   );
 }
 
 export default Signup;
+
+// import React from 'react';
+
+// export default function BasicTextFields() {
+
+//   return (
+
+//   );
+// }
